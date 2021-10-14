5e565ef02f9d0ae7e5b1274274cd86c61c639eba
Roy Kid <lijichen365@126.com>
2021-10-14 18:56:14 +0800

    init repo and add WSL proxy

---
+++ README.md
@@ -0,0 +1,3 @@
+Roy's Scripts and Configuration
+
+1. WSL proxy switch
---
+++ proxy.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+hostip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
+hostip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ")
+wslip=$(hostname -I | awk '{print $1}')
+port=8889
+
+PROXY_HTTP="http://${hostip}:${port}"
+
+set_proxy(){
+
+    export http_proxy="${PROXY_HTTP}"
+    export HTTP_PROXY="${PROXY_HTTP}"
+
+    export https_proxy="${PROXY_HTTP}"
+    export HTTPS_proxy="${PROXY_HTTP}"
+    echo "set proxy: " ${https_proxy}
+}
+
+unset_proxy(){
+    unset http_proxy
+    unset HTTP_PROXY
+    unset https_proxy
+    unset HTTPS_PROXY
+}
+
+test_setting(){
+    echo "Host ip:" ${hostip}
+    echo "WSL ip:" ${wslip}
+    echo "Current proxy:" $https_proxy
+}
+
+if [ "$1" = "set" ]
+then
+    set_proxy
+
+elif [ "$1" = "unset" ]
+then
+    unset_proxy
+
+elif [ "$1" = "test" ]
+then
+    test_setting
+else
+    echo "Unsupported arguments."
+fi
